f49f98d90c76d96617cea1004e1098b0e20fb055
francescoB1997 <francesco.0297@hotmail.it>
2024-10-14 00:36:50 +0200

    Test titolo commit + Test commit descrizione

--- coordinatorController.py
+++ coordinatorController.py
@@ -17,6 +17,7 @@ def main():
         else:
             coordinator_mqtt.send_probe_command(probe_id = "probe1", command = "role: Server")
             coordinator_mqtt.send_probe_command(probe_id = "probe2", command = "role: Client")
+    print("test commit")
 
 if __name__ == "__main__":
     main()
