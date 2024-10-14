03a41ba9443fabf7d8e208f5f00ef8dc7aa6d9ba
francescoB1997 <francesco.0297@hotmail.it>
2024-10-14 13:46:13 +0200

    Modifiche cartelle + create le cartelle per la separazione in moduli delle funzionalità

--- modules/mqttModule/mqttClient.py
+++ modules/mqttModule/mqttClient.py
@@ -1,14 +1,22 @@
 import yaml
 import paho.mqtt.client as mqtt
 
-class mqttClient:
+"""
+    Il file di configurazione sarà uno per ogni client mqtt. Facendo le prove su un'unica macchina, "sono costretto" a definire più
+    di un file di configurazione, uno per il coordinator ed uno per ogni probes, in quanto ci sono parametri "personali" per il client mqtt.
+    Quindi, per adesso i file config si chiameranno "coordinatorConfig.yaml"... quando poi andrò su raspberry, posso anche rinominarli tutti
+    in "config.yaml".
+"""
+
+class mqttClient(mqtt.Client):
+
     def __init__(self):
-        self.client = None
         self.config = None
         self.probes_command_topic = None
 
-        with open("./modules/mqttClient/config.yaml") as file:
+        with open("./modules/mqttModule/coordinatorConfig.yaml") as file:
             self.config = yaml.safe_load(file)
+
         mqtt_config = self.config['mqtt_client']
         client_id = mqtt_config['client_id']
         broker_ip = mqtt_config['broker']['host']
@@ -16,50 +24,35 @@ class mqttClient:
         keep_alive = mqtt_config['broker']['keep_alive']
         self.probes_command_topic = mqtt_config['publishing']['probes_command_topic']
 
-        print("File configurazione mqtt caricato")
-        print(f"mqtt_config: {mqtt_config}")
+        super().__init__(client_id = client_id)
 
-        self.client = mqtt.Client(client_id = client_id)
-        self.client.on_connect = self.connection_success_event_handler
-        self.client.on_message = self.new_message_event_handler
+        self.on_connect = self.connection_success_event_handler
+        self.on_message = self.new_message_event_handler
         if(mqtt_config['broker']['login']): # If there is the enabled credentials ...
-            self.client.username_pw_set(
+            self.username_pw_set(
                 mqtt_config['credentials']['username'],
                 mqtt_config['credentials']['password'])
         
-        self.client.connect(broker_ip, broker_port, keep_alive)
-        self.client.loop_start()
+        self.connect(broker_ip, broker_port, keep_alive)
+        self.loop_start()
 
     def connection_success_event_handler(self, client, userdata, flags, rc): 
         # Invoked when the connection to broker has success
-        if(self.client is None):
-            return
         print(f"The broker has accepted the connection...")
         for topic in self.config['mqtt_client']['subscription_topics']:
-            self.client.subscribe(topic)
+            self.subscribe(topic)
             print(f"Subscription to topic [{topic}]")
 
     def new_message_event_handler(self, client, userdata, message):
-        # Invoked when a new message has arrived from the broker
-        if(self.client is None):
-            return        
+        # Invoked when a new message has arrived from the broker      
         print(f"Message pusblished on topic: {message.topic} -> {message.payload.decode('utf-8')}")
-
-    def publish_message(self, topic, message):
-        # Invoked when you want publish a message on a topic
-        if(self.client is None):
-            return
-        self.client.publish(topic, message)
 
     def send_probe_command(self, probe_id, command):
         # Invoked when you want to send a command to one probe
-        self.publish_message("probes/" + probe_id + "/" + self.probes_command_topic, command)        
+        self.publish("probes/" + probe_id + "/" + self.probes_command_topic, command)        
 
     def disconnect(self):
         # Invoked to inform the broker to release the allocated resources
-        if(self.client is None):
-            return
-        self.client.loop_stop()
-        self.client.disconnect()
-        self.client = None
+        self.loop_stop()
+        super().disconnect()
         print(f"Disconnected")
