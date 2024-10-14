cb7cc3600c189abd9df49c089104e510afd720c3
francescoB1997 <francesco.0297@hotmail.it>
2024-10-14 13:48:19 +0200

    Estensione della classe paho-mqtt + Ho deciso di estendere le funzionalità della classe in titolo, così da aggiungere le funzionalità opportune in base alla classe figlia

---
+++ modules/probesFirmware/mqttModule/mqttClient.py
@@ -0,0 +1,75 @@
+import yaml
+import paho.mqtt.client as mqtt
+
+"""
+    Estensione della classe paho.mqtt per l'utilizzo sulle probes
+"""
+
+class mqttClient(mqtt.Client):
+
+    def __init__(self, probe_id):
+        self.config = None
+        self.probes_command_topic = None
+        self.probe_id = None
+        self.status_topic = None
+        self.msg_topic = None
+        self.role = None
+
+        yaml_path = "./modules/probesFirmware/mqttModule/" + probe_id + ".yaml"
+        with open(yaml_path) as file:
+            self.config = yaml.safe_load(file)
+
+        self.config = self.config['mqtt_client']
+        self.probe_id = self.config['probe_id']
+        broker_ip = self.config['broker']['host']
+        broker_port = self.config['broker']['port']
+        keep_alive = self.config['broker']['keep_alive']
+        self.status_topic = str(self.config['publishing']['status_topic']).replace('PROBE_ID', self.probe_id)
+        self.msg_topic = str(self.config['publishing']['message_topic']).replace('PROBE_ID', self.probe_id)
+
+        super().__init__(client_id = self.probe_id)
+
+        self.on_connect = self.connection_success_event_handler
+        self.on_message = self.new_message_event_handler
+        if(self.config['broker']['login']): # If there is the enabled credentials ...
+            self.username_pw_set(
+                self.config['credentials']['username'],
+                self.config['credentials']['password'])
+        
+        self.connect(broker_ip, broker_port, keep_alive)
+        self.loop_start()
+
+    def connection_success_event_handler(self, client, userdata, flags, rc): 
+        # Invoked when the connection to broker has success
+        print(f"The broker has accepted the connection... Publish my ONLINE status")
+        self.publish_status("ONLINE")
+        for topic in self.config['subscription_topics']:
+            topic = str(topic).replace("PROBE_ID", self.probe_id) # Substitution the "PROBE_ID" element with the real probe id
+            self.subscribe(topic)
+            print(f"Subscription to topic [{topic}]")
+
+    def new_message_event_handler(self, client, userdata, message):
+        # Invoked when a new message has arrived from the broker      
+        print(f"Message pusblished on topic: {message.topic} -> {message.payload.decode('utf-8')}")
+
+    def publish_status(self, status):
+        # Invoked when you want to publish your status
+        self.publish(
+            topic = self.status_topic,
+            payload = status,
+            qos = self.config['publishing']['qos'],
+            retain = self.config['publishing']['retain'] )
+        
+    def publish_msg(self, msg):
+        # Invoked when you want to publish a msg
+        self.publish(
+            topic = self.msg_topic,
+            payload = msg,
+            qos = self.config['publishing']['qos'],
+            retain = self.config['publishing']['retain'] )
+
+    def disconnect(self):
+        # Invoked to inform the broker to release the allocated resources
+        self.loop_stop()
+        super().disconnect()
+        print(f"Disconnected")
