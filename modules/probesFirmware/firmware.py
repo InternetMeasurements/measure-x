d2d9381df3f48e25d21dbd97cfc8aeb9b7f883ce
francescoB1997 <francesco.0297@hotmail.it>
2024-10-14 17:33:04 +0200

    Semplice firmware

--- modules/probesFirmware/firmware.py
+++ modules/probesFirmware/firmware.py
@@ -2,21 +2,19 @@ from modules.probesFirmware.mqttModule.mqttClient import mqttClient
 
 def main():
     probe1Mqtt = mqttClient("probe1")
-    #probe2Mqtt = mqttClient("probe2")
+    probe2Mqtt = mqttClient("probe2")
     while True:
         command = input()
         if(command == '0'):
             probe1Mqtt.publish_status("OFFLINE")
-            #probe2Mqtt.publish_status("OFFLINE")
+            probe2Mqtt.publish_status("OFFLINE")
             probe1Mqtt.disconnect()
-            #probe2Mqtt.disconnect()
+            probe2Mqtt.disconnect()
             break
         else:
             probe1Mqtt.publish_msg("role?")
-            #probe2Mqtt.publish_msg("role?")
+            probe2Mqtt.publish_msg("role?")
     return
-
-
 
 if __name__ == "__main__":
     main()
