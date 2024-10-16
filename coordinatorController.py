9489ab97c4c8d5ba9bce0120f607c5fa64f554da
francescoB1997 <francesco.0297@hotmail.it>
2024-10-16 09:46:11 +0200

    Aggiunto import modulo per il Background Traffic

--- coordinatorController.py
+++ coordinatorController.py
@@ -1,11 +1,13 @@
 from modules.iperfClient.iperfController import IperfController
 from modules.mqttModule.mqttClient import MqttClient
+from modules.backgroundTrafficModule.backgroundTrafficGenerator import BackgroundTrafficGenerator
 
 def main():
 
     #iperf_controller = IperfController()
     #iperf_controller.run_iperf_repetitions()
 
+    """
     coordinator_mqtt = MqttClient()
     while True:
         command = input()
@@ -15,6 +17,10 @@ def main():
         else:
             coordinator_mqtt.send_probe_command(probe_id = "probe1", command = "role: Server")
             coordinator_mqtt.send_probe_command(probe_id = "probe2", command = "role: Client")
+    """
+    bt_generator = BackgroundTrafficGenerator()
+    bt_generator.submit_process("./modules/backgroundTrafficModule/pcap_files/pcapESPMoistrue.pcap")
+    bt_generator.execute_process()
 
 if __name__ == "__main__":
     main()
