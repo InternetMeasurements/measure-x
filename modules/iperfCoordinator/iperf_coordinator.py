9b94c4ec388c3b99e5eafc2585cc6c47f395dce7
francescoB1997 <francesco.0297@hotmail.it>
2024-10-23 13:30:52 +0200

    Definito il formato dei messaggi JSON + Probes e Coordinator si scambiano messaggi JSON in formato definito, composto da un primo livello che indica a chi è destinato tale messaggio, e poi un payload che rappresenta il secondo livello. All'interno del payload quindi ci sono sotto-comandi o informazioni di stato delle sonde. + L'IperfCoordinator deve avere per ogni sonda una sorta di file yaml di configurazione "generale". Vediamo se diventa solamente uno per tutte le sonde

--- modules/iperfCoordinator/iperf_coordinator.py
+++ modules/iperfCoordinator/iperf_coordinator.py
@@ -1,5 +1,6 @@
 import os
 import json
+import yaml
 from pathlib import Path
 from src.modules.mqttModule.mqttClient import MqttClient
 
@@ -8,29 +9,77 @@ class Iperf_Coordinator:
         self.mqtt = mqtt 
         self.received_acks = set()
         self.expected_acks = set()
+        self.probes_configurations_dir = 'probes_configurations'
         self.last_client_probe = None
+        self.probes_servers_ip = {}
+        self.probes_servers_port = {}
 
     def result_handler_received(self, probe_sender, result: json):
         self.store_measurement_result(probe_sender, result)
         
-    def status_handler_rcvd(self, probe_sender, status : str):
-        if status.__contains__("conf: OK"):
-            if probe_sender in self.expected_acks:
-                self.received_acks.add(probe_sender)
-                print(f"iperf_coordinator: ACK received for probe -> {probe_sender}")
-        else:
-            print(f"iperf_coordinator: NACK received for {probe_sender} -> {status}")
+    def status_handler_received(self, probe_sender, status : json):
+        command = status["command"]
+        match command:
+            case "conf":
+                if "reason" not in status: # If true, then this status is an ACK
+                    if ("ip" in status) and ("port" in status):
+                        probe_ip = status["ip"]
+                        probe_port = status["port"]
+                        self.probes_ip[probe_sender] = probe_ip
+                        self.probes_servers_port[probe_sender] = probe_port
+                    print(f"Iperf_Coordinator: probe |{probe_sender}|->|{probe_ip}|->|{command}|->ACK")
+                else: # else, this is an NACK
+                    reason = status['reason']
+                    print(f"Iperf_Coordinator: probe |{probe_sender}|->|{command}|->NACK, reason->{reason}")
+            case _:
+                print(f"Iperf_Coordinator: status message not handled-> |{command}|")
+
+    def get_json_from_probe_yaml(self, probes_configurations_path) -> json:
+        json_probe_config = {}
+        with open(probes_configurations_path, "r") as file:
+            config = yaml.safe_load(file)
+            json_probe_config = {
+                "destination_server_ip": config['destination_server_ip'],
+                "destination_server_port": int(config['destination_server_port']),
+                "transport_protocol": True if (config['transport_protocol'] == "TCP") else False,
+                "parallel_connections": int(config['parallel_connections']),
+                "result_measurement_filename": config['result_measurement_filename'],
+                "reverse": config['reverse'],
+                "verbose": False,
+                "total_repetition": int(config['total_repetition']),
+            }
+        return json_probe_config
 
 
-    def send_probe_iperf_role(self, probe_id, role):
+    # VECCHIA send_probe_iperf_role
+    def send_probe_iperf_configuration(self, probe_id, role, dest_probe = None): # Tramite il probe_id, devi caricare il file YAML per quella probes
+        json_config = {}
         if role == "Client":
-            self.last_client_probe = probe_id
+            base_path = Path(__file__).parent
+            probes_configurations_path = Path(os.path.join(base_path, self.probes_configurations_dir, (probe_id + ".yaml")))
+            if probes_configurations_path.exists():
+                json_config = self.get_json_from_probe_yaml(probes_configurations_path)
+                if (dest_probe not in self.probes_servers_ip) or (dest_probe not in self.probes_servers_port):
+                    print(f"Iperf_Coordinator: configure the Probe Server first")
+                    return
+                json_config['destination_server_ip'] = self.probes_servers_ip[dest_probe]
+                json_config['destination_server_port'] = self.probes_servers_port[dest_probe]
+                self.last_client_probe = probe_id
+        else:
+            # Questo json_config è quello di default per il Server.
+            json_config = {
+                "listen_port": 5201,
+                "verbose": True
+            }
         self.expected_acks.add(probe_id) # Add this probe in the list from which i'm expecting an ACK
-        command_role = "iperf: role=" + role
-        self.mqtt.publish_command(command = command_role, probe_id = probe_id)
+        json_command = {
+            "handler": 'iperf',
+            "command": "conf",
+            "payload": json_config
+        }
+        self.mqtt.publish_on_command_topic(probe_id=probe_id, complete_command=json.dumps(json_command))
         
     def send_probe_iperf_start(self):
-        """
         if self.expected_acks == set():
             print("iperf_coordinator: expected_acks empty")
             return
@@ -38,14 +87,19 @@ class Iperf_Coordinator:
         if self.expected_acks != self.received_acks:
             print(f"iperf_coordinator: Can't iperf start. Waiting for {self.expected_acks - self.received_acks} role ACK")
             return
-        """
+
         if self.last_client_probe is None:
             print("iperf_coordinator: Did you configured the probes first?")
             return
+        
+        json_iperf_start = {
+            "handler": "iperf",
+            "command": "start",
+            "payload": {}
+        }
     
-        command_iperf_start = "iperf: start"
-        self.mqtt.publish_command(command = command_iperf_start, probe_id = self.last_client_probe)
-        print("iperf_coordinator: iperf started on probes. Waiting for results...")
+        self.mqtt.publish_on_command_topic(probe_id = self.last_client_probe, command = json.dumps(json_iperf_start))
+        print("Iperf_Coordinator: iperf started on probes. Waiting for results...")
 
     def store_measurement_result(self, probe_sender, json_measurement: json):
         base_path = Path(__file__).parent
@@ -55,4 +109,4 @@ class Iperf_Coordinator:
             os.makedirs(probe_measurement_dir, exist_ok=True)
         with open(complete_measurement_path, "w") as file:
             file.write(json.dumps(json_measurement, indent=4))
-        print(f"iperf_coordinator: stored result from {probe_sender} -> measure_{str(json_measurement['measurement_id'])}.json")
+        print(f"Iperf_Coordinator: stored result from {probe_sender} -> measure_{str(json_measurement['measurement_id'])}.json")
