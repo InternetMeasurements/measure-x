507acaa4e9b1d5ec0c25a610ae8fc7fa942ae368
francescoB1997 <francesco.0297@hotmail.it>
2024-10-24 11:59:44 +0200

    Corretta lettura messaggi Json + La lettura dei messaggi json non rispettava il formato definito.

--- probesFirmware/commandsMultiplexer/commandsMultiplexer.py
+++ probesFirmware/commandsMultiplexer/commandsMultiplexer.py
@@ -1,15 +1,17 @@
 # Classe che rappresenta la funzionalità di decodifica ed esecuzione dei vari moduli sulle porbes
+import json
 
 class CommandsMultiplexer():
     def __init__(self):
         self.commands_handler_list = {}
+        #self.status_handler_list = {} # For the future. Is necessary that the probe knows about the Coordinator state? Or another probe state?
 
     # +------------------ commands_handler_list ------------------+
     # |      COMMAND     |          HANDLER FUNCTION              |
     # +------------------+----------------------------------------+
     # |       iperf      |  iperfController.iperf_command_handler |
     # |       ping       |                                        |
-    # |   bg_generaror   |                                        |
+    # |   bg_generator   |                                        |
     # +------------------+----------------------------------------+
 
     def registration_handler_request(self, interested_command, handler) -> str:
@@ -17,14 +19,16 @@ class CommandsMultiplexer():
             self.commands_handler_list[interested_command] = handler
             return "OK"
         else:
-            return "There is already a registered handler for -> " + interested_command
+            return "There is already a registered handler for " + interested_command
 
-    def decode_command(self, complete_command : str):
+    def decode_command(self, complete_command):
+        nested_command = json.loads(complete_command)
         # Il Decode command deve interpretare il JSON
-        print(f"command_multiplexer: complete_command -> {complete_command} ")
-        command_key = complete_command.split(':')[0]
-        command_value = complete_command.split(':')[1].replace(' ', '')
-        if command_key in self.commands_handler_list:
-            self.commands_handler_list[command_key](command_value)
+        print(f"command_multiplexer: complete_command -> {nested_command} ")
+        handler = nested_command["handler"]
+        command = nested_command["command"]
+        payload = nested_command["payload"]
+        if handler in self.commands_handler_list:
+            self.commands_handler_list[handler](command, payload)
         else:
-            print(f"CommandsMultiplexer: no registered handler for {command_key}")
+            print(f"CommandsMultiplexer: no registered handler for {handler}")
