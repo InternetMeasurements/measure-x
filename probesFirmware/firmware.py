474a52ba0e538460fbf56244ceef8db30737fc31
francescoB1997 <francesco.0297@hotmail.it>
2024-10-29 13:35:33 +0100

    Aggiunta creazione oggetto PingController + Nel main della probe, ho allocato l'oggetto PingController per implementare nella porbe anche la funzionalità di misurazione di latenza. + l'oggetto PingController si registrerà presso il CommandsMultiplexer per poter ricevere tutti i messaggi il cui handler è 'ping'

--- probesFirmware/firmware.py
+++ probesFirmware/firmware.py
@@ -1,15 +1,19 @@
 from src.probesFirmware.mqttModule.mqttClient import ProbeMqttClient
 from src.probesFirmware.commandsMultiplexer.commandsMultiplexer import CommandsMultiplexer
 from src.probesFirmware.iperfClient.iperfController import IperfController
+from src.probesFirmware.latencyModule.pingController import PingController
 
 class Probe:
     def __init__(self, probe_id):
         self.id = probe_id
         self.state = None
         self.commands_multiplexer = CommandsMultiplexer()
-        self.mqtt_client = ProbeMqttClient(probe_id, self.commands_multiplexer.decode_command) # The Decode Handler is triggered internally
-        self.iperf_controller = IperfController(self.mqtt_client, self.commands_multiplexer.registration_handler_request)
-
+        self.mqtt_client = ProbeMqttClient(probe_id,
+                                           self.commands_multiplexer.decode_command) # The Decode Handler is triggered internally
+        self.iperf_controller = IperfController(self.mqtt_client,
+                                                self.commands_multiplexer.registration_handler_request) # ENABLE THROUGHPUT FUNCTIONALITY
+        self.ping_controller = PingController(self.mqtt_client,
+                                              self.commands_multiplexer.registration_handler_request)   # ENABLE LATENCY FUNCTIONALITY
         
     def check_for_ready(self):
         return self.state
