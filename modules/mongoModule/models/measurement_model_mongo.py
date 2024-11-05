5fe59b0b5769d6506602a6c6ef1e68f22dd192aa
francescoB1997 <francesco.0297@hotmail.it>
2024-11-05 19:38:46 +0100

    Modello dati per un Measurement + Il modello rappresenta i dati da memorizzare quando si sta per avviare una misurazione di una qualche metrica.

---
+++ modules/mongoModule/models/measurement_model_mongo.py
@@ -0,0 +1,32 @@
+from datetime import datetime as dt
+from src.modules.mongoModule.models.background_traffic_model_mongo import BackgroundTrafficModelMongo
+
+class MeasurementModelMongo:
+    def __init__(self, description, source_probe, dest_probe,source_probe_ip, dest_probe_ip, gps_source_probe = None, gps_dest_probe = None) -> None:
+        self._id = None
+        self.description = description
+        self.type = None
+        self.creation_time = dt.now()
+        self.source_probe = source_probe
+        self.dest_probe = dest_probe
+        self.source_probe_ip = source_probe_ip,
+        self.dest_probe_ip = dest_probe_ip,
+        self.gps_source_probe = gps_source_probe
+        self.gps_dest_probe = gps_dest_probe
+        self.background_traffic = None #BackgroundTrafficModelMongo()
+        self.stop_time = None
+
+    def to_dict(self):
+        return {
+            'description': self.description,
+            'type': self.type,
+            'creation_time': self.creation_time,
+            'source_probe': self.source_probe,
+            'dest_probe': self.dest_probe,
+            'source_probe_ip': self.source_probe_ip,
+            'dest_probe_ip': self.dest_probe_ip,
+            'gps_source_probe': self.gps_source_probe,
+            'gps_dest_probe': self.gps_dest_probe,
+            'background_traffic': self.background_traffic,
+            'stop_time': self.stop_time
+        }
