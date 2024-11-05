58ebbb31869f60dae468acbba409e0424df48da1
francescoB1997 <francesco.0297@hotmail.it>
2024-11-05 09:39:38 +0100

    Implementazione interfaccia verso MongoDB + La classe implementa le funzionalità di memorizzazione verso il database mongoDB per conservare le varie misurazioni e risultati ottenuti dagli esperimenti futuri. + Per adesso, il mongoServer risiede nella stessa macchina che esegue il coordinator

--- modules/mongoModule/mongoDB.py
+++ modules/mongoModule/mongoDB.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from src.modules.mongoModule.measurement_model_mongo import MeasurementModelMongo, BackgroundTrafficModelMongo
 
 class MongoDB:
     def __init__(self):
@@ -6,35 +7,59 @@ class MongoDB:
         self.user = "measurex"
         self.password = "measurex"
         self.db_name = "measurex"
-        self.results_collection_name = "results"
-        self.measurements_collection_name = "measurements"
+        self.results_collection = None
+        self.measurements_collection = None
         #self.client = MongoClient("mongodb://" + self.user + ":" + self.password + "@" + self.addr_mongo_server + ":27017/" + self.db_name)
         self.client = MongoClient("mongodb://measurex:measurex@192.168.1.102:27017/")
 
         db = self.client[self.db_name] # crea il db measurex
 
-        if self.measurements_collection_name not in db.list_collection_names():
-            db.create_collection(self.measurements_collection_name)
+        if "measurements" not in db.list_collection_names(): # if the measurements_collection doesn't exists, then creates it
+            db.create_collection("measurements")
 
-        if self.results_collection_name not in db.list_collection_names():
-            db.create_collection(self.results_collection_name)
+        if "results" not in db.list_collection_names():  # if the results_collection doesn't exists, then creates it
+            db.create_collection("results")
         
-        measurements_collection = db["measurements"]
-        results_collection = db["results"]
+        self.measurements_collection = db["measurements"]
+        self.results_collection = db["results"]
 
-        measurements_collection.insert_one({})
-        results_collection.insert_one()
-
-        print("Collezioni create:")
+        #measurements_collection.insert_one({})
+        #results_collection.insert_one()
+        """
+        print("Collezioni esistenti: ")
         print(db.list_collection_names())
-"""
-        measurements_data = measurements_collection.find()
+
+        measurements_data = self.measurements_collection.find()
         print("Dati dalla collezione 'measurements':")
         for measurement in measurements_data:
-            print(measurement)
+            print(f"id: {measurement['_id']}")
+            delete_result = measurements_collection.delete_one({"_id": measurement['_id']})
+            if delete_result.deleted_count > 0:
+                print("eliminato")
+            else:
+                print("non eliminato")
 
         results_data = results_collection.find()
         print("\nDati dalla collezione 'results':")
         for result in results_data:
             print(result)
-"""
+            delete_result = results_collection.delete_one({'_id': result['_id']})
+            if delete_result.deleted_count > 0:
+                print("eliminato")
+            else:
+                print("non eliminato")
+        """
+    
+    def insert_measurement(self, measure : MeasurementModelMongo) -> bool:
+        try:
+            insert_result = self.measurements_collection.insert_one(measure.to_dict())
+            if insert_result.inserted_id:
+                print(f"Measurement stored in mongo. ID -> |{insert_result.inserted_id}|")
+                return True
+        except Exception as e:
+            print(f"Error while storing the measurment on mongo -> {e}")
+            return False
+    
+
+    def delete_result_by_id(id : str) -> bool:
+        return False
