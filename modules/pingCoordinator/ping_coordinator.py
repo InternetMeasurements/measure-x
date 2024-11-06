ea436d72c030160ac8777446bc7df6bb229b9957
francescoB1997 <francesco.0297@hotmail.it>
2024-11-06 13:14:17 +0100

    Memorizzazione del Ping Result + Il metodo store_measurement_result() implementa la memorizzazione del risultato appena ricevuto dalla probe.

--- modules/pingCoordinator/ping_coordinator.py
+++ modules/pingCoordinator/ping_coordinator.py
@@ -84,12 +84,31 @@ class Ping_Coordinator:
         }
         self.mqtt_client.publish_on_command_topic(probe_id=probe_destination, complete_command=json.dumps(json_ping_stop))
 
-    def store_measurement_result(self, probe_sender, result):
-        return
+
+    def store_measurement_result(self, result : json):
+        mongo_result = PingResultModelMongo(
+            measure_reference = ObjectId(result["measure_reference"]),
+            start_timestamp = result["start_timestamp"],
+            rtt_avg = result["rtt_avg"],
+            rtt_max = result["rtt_max"],
+            rtt_min = result["rtt_min"],
+            rtt_mdev = result["rtt_mdev"],
+            packets_sent = result["packet_transmit"],
+            packets_received = result["packet_receive"],
+            packets_loss_count = result["packet_loss_count"],
+            packets_loss_rate = result["packet_loss_rate"],
+            icmp_replies = result["icmp_replies"]
+        )
+        result_id = str(self.mongo_db.insert_ping_result(result=mongo_result))
+        if result_id is not None:
+            print(f"Iperf_Coordinator: result |{result_id}| stored in db")
+        else:
+            print(f"Iperf_Coordinator: error while storing result |{result_id}|")
+
 
     def print_summary_result(self, measurement_result):
         start_timestamp = measurement_result["start_timestamp"]
-        measurement_id = measurement_result["measurement_id"]
+        measure_reference = measurement_result["measure_reference"]
         source_ip = measurement_result["source"]
         destination_ip = measurement_result["destination"]
         packets_transmitted = measurement_result["packet_transmit"]
@@ -101,9 +120,9 @@ class Ping_Coordinator:
         rtt_max = measurement_result["rtt_max"]
         rtt_mdev = measurement_result["rtt_mdev"]
 
-        print("\n****************** SUMMARY ******************")
+        print("\n****************** PING SUMMARY ******************")
         print(f"Timestamp: {start_timestamp}")
-        print(f"Measurement ID: {measurement_id}")
+        print(f"Measurement ID: {measure_reference}")
         print(f"IP sorgente: {source_ip}")
         print(f"IP destinatario: {destination_ip}")
         print(f"Packets trasmitted: {packets_transmitted}")
