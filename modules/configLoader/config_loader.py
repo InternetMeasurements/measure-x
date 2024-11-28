f2d3d46434c554b639bc2af68e6c674548717df6
francescoB1997 <francesco.0297@hotmail.it>
2024-11-28 22:01:58 +0100

    Modifica costruttore + Il costruttore ConfigLoader() riceve dall'esterno il path e nome file in cui caricare la configurazione

--- modules/configLoader/config_loader.py
+++ modules/configLoader/config_loader.py
@@ -1,15 +1,21 @@
 import yaml
 import os
-from pathlib import Path
 from types import SimpleNamespace
 
-class ConfigLoader:
-    def __init__(self) -> None:
-        self.mongo_config = None
+MONGO_KEY = 'mongo'
+MQTT_KEY = 'mqtt_client'
 
-        base_path = Path(__file__).parent
-        yaml_dir = os.path.join(base_path, "coordinator_conf.yaml")
+class ConfigLoader:
+    def __init__(self, base_path, file_name):
+        self.mongo_config = None
+        self.mqtt_config = None
+
+        #base_path = Path(__file__).parent
+        #yaml_dir = os.path.join(base_path, "coordinator_conf.yaml")
+        yaml_dir = os.path.join(base_path, file_name)
         with open(yaml_dir) as file:
             self.config = yaml.safe_load(file)
-        if 'mongo' in self.config:
-            self.mongo_config = SimpleNamespace(**self.config['mongo'])
+        if MONGO_KEY in self.config:
+            self.mongo_config = SimpleNamespace(**self.config[MONGO_KEY])
+        if MQTT_KEY in self.config:
+            self.mqtt_config = self.config
