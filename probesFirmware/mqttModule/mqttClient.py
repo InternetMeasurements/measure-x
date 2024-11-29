0aee8bff1d1ccfb9f5f162ac32424ff5ca9dafd8
francescoB1997 <francesco.0297@hotmail.it>
2024-11-29 20:37:18 +0100

    Correzione individuazione esecuzione su raspberry

--- probesFirmware/mqttModule/mqttClient.py
+++ probesFirmware/mqttModule/mqttClient.py
@@ -141,7 +141,7 @@ class ProbeMqttClient(mqtt.Client):
         if (state == "ONLINE") or (state == "UPDATE"):
             hostname = socket.gethostname()
             my_ip = socket.gethostbyname(hostname)
-            if my_ip == "127.0.0.1":
+            if my_ip == "127.0.1.1":
                 my_ip = netifaces.ifaddresses(INTERFACE_NAME)[netifaces.AF_INET][0]['addr']
             #my_ip = netifaces.ifaddresses(netifaces.interfaces()[0])[netifaces.AF_INET][0]['addr']
             json_status["payload"]["ip"] = my_ip
