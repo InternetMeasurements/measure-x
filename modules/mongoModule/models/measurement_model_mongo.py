b66b25b50425760457d2e58094b898d0e2d3212f
francescoB1997 <francesco.0297@hotmail.it>
2025-01-18 13:11:10 +0100

    Added static method cast_dic_in_MeasurementModelMongo + The method, received as parameter a measurement_dic (coming from mongo), cast it in a MeasurementModelMongo object

--- modules/mongoModule/models/measurement_model_mongo.py
+++ modules/mongoModule/models/measurement_model_mongo.py
@@ -1,9 +1,10 @@
 from modules.mongoModule.models.coexisting_application_model_mongo import CoexistingApplicationModelMongo
 
 class MeasurementModelMongo:
-    def __init__(self, description, type, source_probe, dest_probe, source_probe_ip, dest_probe_ip, 
-                 state = None, start_time = None, gps_source_probe = None, gps_dest_probe = None):
-        self._id = None
+    def __init__(self, description, type, source_probe, dest_probe, source_probe_ip, dest_probe_ip, _id = None,
+                 state = None, start_time = None, gps_source_probe = None, gps_dest_probe = None,
+                 coexisting_application = None, stop_time = None):
+        self._id = _id
         self.description = description
         self.type = type
         self.state = state
@@ -14,8 +15,36 @@ class MeasurementModelMongo:
         self.dest_probe_ip = dest_probe_ip,
         self.gps_source_probe = gps_source_probe
         self.gps_dest_probe = gps_dest_probe
-        self.coexisting_application = None #CoexistingApplicationModelMongo()
-        self.stop_time = None
+        self.coexisting_application = coexisting_application #CoexistingApplicationModelMongo()
+        self.stop_time = stop_time
+
+    @staticmethod
+    def cast_dic_in_MeasurementModelMongo(measurement_as_dic):
+        _id = measurement_as_dic['_id']
+        description = measurement_as_dic['description']
+        type = measurement_as_dic['type']
+        source_probe = measurement_as_dic['source_probe']
+        dest_probe = measurement_as_dic['dest_probe']
+        source_probe_ip = measurement_as_dic['source_probe_ip']
+        dest_probe_ip = measurement_as_dic['dest_probe_ip']
+        state = start_time = gps_source_probe = gps_dest_probe = coexisting_application = stop_time = None 
+        if 'state' in measurement_as_dic:
+            state = measurement_as_dic['state']
+        if 'start_time' in measurement_as_dic:
+            start_time = measurement_as_dic['start_time']
+        if 'gps_source_probe' in measurement_as_dic:
+            gps_source_probe = measurement_as_dic['gps_source_probe']
+        if 'gps_dest_probe' in measurement_as_dic:
+            gps_dest_probe = measurement_as_dic['gps_dest_probe']
+        if 'coexisting_application' in measurement_as_dic:
+            coexisting_application = measurement_as_dic['coexisting_application']
+        if 'stop_time' in measurement_as_dic:
+            stop_time = measurement_as_dic['stop_time']
+        measurement_to_return = MeasurementModelMongo(description=description, type=type, source_probe=source_probe, _id=_id,
+                                                      dest_probe=dest_probe, source_probe_ip=source_probe_ip, dest_probe_ip=dest_probe_ip,
+                                                      state=state, start_time=start_time, gps_source_probe=gps_source_probe, gps_dest_probe=gps_dest_probe,
+                                                      coexisting_application=coexisting_application, stop_time=stop_time)
+        return measurement_to_return
 
     def to_dict(self):
         return {
@@ -29,6 +58,6 @@ class MeasurementModelMongo:
             'dest_probe_ip': self.dest_probe_ip,
             'gps_source_probe': self.gps_source_probe,
             'gps_dest_probe': self.gps_dest_probe,
-            'background_traffic': self.background_traffic,
+            'coexisting_application': self.coexisting_application,
             'stop_time': self.stop_time
         }
