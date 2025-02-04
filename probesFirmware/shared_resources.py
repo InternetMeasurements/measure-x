db2798702ccc3143f82b42ffe5828f32691fca7d
francescoB1997 <francesco.0297@hotmail.it>
2025-02-04 09:38:56 +0100

    Updated the handling of Shared State

--- probesFirmware/shared_resources.py
+++ probesFirmware/shared_resources.py
@@ -1,29 +1,30 @@
 import threading
 
+BUSY = "BUSY"
+READY = "READY"
+
 class SharedState:
     def __init__(self):
         self.lock = threading.Lock()
-        self.BUSY = "BUSY"
-        self.READY = "READY"
-        self.probe_state = self.READY
+        self.probe_state = READY
 
     def set_probe_as_ready(self) -> bool:
         with self.lock:
-            self.probe_state = self.READY
-            print(f"SharedState: state setted to |{self.READY}|")
+            self.probe_state = READY
+            print(f"SharedState: state setted to |{READY}|")
             return True
 
     def set_probe_as_busy(self) -> bool:
         with self.lock:
-            if self.probe_state == self.BUSY:
+            if self.probe_state == BUSY:
                 print(f"SharedState: the probe is already busy")
                 return False
-            self.probe_state = self.READY
-            print(f"SharedState: state setted to |{self.BUSY}|")
+            self.probe_state = BUSY
+            print(f"SharedState: state setted to |{BUSY}|")
             return True
         
     def probe_is_ready(self) -> bool:
         with self.lock:
-            return (self.probe_state == self.READY)
+            return (self.probe_state == READY)
 
 shared_state = SharedState() # my singleton shared
