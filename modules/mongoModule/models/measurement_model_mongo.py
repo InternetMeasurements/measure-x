fb1eb713dfa852efa4961a01308a9314536394f6
francescoB1997 <francesco.0297@hotmail.it>
2025-02-04 09:50:07 +0100

    Used the Document Linking between Results e Measurements collections + For this reason, there will be the results array in the Measurement model, in which will be stored all the _id of the relative results

--- modules/mongoModule/models/measurement_model_mongo.py
+++ modules/mongoModule/models/measurement_model_mongo.py
@@ -19,32 +19,38 @@ class MeasurementModelMongo:
         self.gps_dest_probe = gps_dest_probe
         self.coexisting_application = coexisting_application #CoexistingApplicationModelMongo()
         self.stop_time = stop_time
-        self.results = results
+        self.results = [] if results is None else results
+
 
     @staticmethod
-    def cast_dict_in_MeasurementModelMongo(measurement_as_dic):
-        _id = measurement_as_dic['_id']
-        description = measurement_as_dic['description']
-        type = measurement_as_dic['type']
-        source_probe = measurement_as_dic['source_probe']
-        dest_probe = measurement_as_dic['dest_probe']
-        source_probe_ip = measurement_as_dic['source_probe_ip']
-        dest_probe_ip = measurement_as_dic['dest_probe_ip']
-        state = start_time = gps_source_probe = gps_dest_probe = coexisting_application = stop_time = None 
-        if 'state' in measurement_as_dic:
-            state = measurement_as_dic['state']
-        if 'start_time' in measurement_as_dic:
-            start_time = measurement_as_dic['start_time']
-        if 'gps_source_probe' in measurement_as_dic:
-            gps_source_probe = measurement_as_dic['gps_source_probe']
-        if 'gps_dest_probe' in measurement_as_dic:
-            gps_dest_probe = measurement_as_dic['gps_dest_probe']
-        if 'coexisting_application' in measurement_as_dic:
-            coexisting_application = measurement_as_dic['coexisting_application']
-        if 'stop_time' in measurement_as_dic:
-            stop_time = measurement_as_dic['stop_time']
-        if 'results' in measurement_as_dic:
-            results = measurement_as_dic['results']
+    def cast_dict_in_MeasurementModelMongo(measurement_as_dict):
+        try:
+            type = measurement_as_dict['type']
+            source_probe = measurement_as_dict['source_probe']
+            dest_probe = measurement_as_dict['dest_probe']
+            source_probe_ip = measurement_as_dict['source_probe_ip']
+            dest_probe_ip = measurement_as_dict['dest_probe_ip']
+        except Exception as e:
+            return None
+        _id = state = description = start_time = gps_source_probe = gps_dest_probe = coexisting_application = stop_time = results = None 
+        if ('_id' in measurement_as_dict):
+            _id = measurement_as_dict['_id']
+        if 'state' in measurement_as_dict:
+            state = measurement_as_dict['state']
+        if 'description' in measurement_as_dict:
+            description = measurement_as_dict['description']
+        if 'start_time' in measurement_as_dict:
+            start_time = measurement_as_dict['start_time']
+        if 'gps_source_probe' in measurement_as_dict:
+            gps_source_probe = measurement_as_dict['gps_source_probe']
+        if 'gps_dest_probe' in measurement_as_dict:
+            gps_dest_probe = measurement_as_dict['gps_dest_probe']
+        if 'coexisting_application' in measurement_as_dict:
+            coexisting_application = measurement_as_dict['coexisting_application']
+        if 'stop_time' in measurement_as_dict:
+            stop_time = measurement_as_dict['stop_time']
+        if 'results' in measurement_as_dict:
+            results = measurement_as_dict['results']
         measurement_to_return = MeasurementModelMongo(description=description, type=type, source_probe=source_probe, _id=_id,
                                                       dest_probe=dest_probe, source_probe_ip=source_probe_ip, dest_probe_ip=dest_probe_ip,
                                                       state=state, start_time=start_time, gps_source_probe=gps_source_probe, gps_dest_probe=gps_dest_probe,
@@ -56,7 +62,7 @@ class MeasurementModelMongo:
 
     def to_dict(self):
         return {
-            '_id' : str(self._id),
+            '_id' : self._id,
             'description': self.description,
             'type': self.type,
             'state': self.state,
