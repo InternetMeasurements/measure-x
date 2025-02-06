5ec6e18a2878f88968ed3069fd9920dac4c53583
francescoB1997 <francesco.0297@hotmail.it>
2025-02-06 19:01:25 +0100

    Automated Ping Measurements + Now, also the latency funcionality is handled in auto mode from the coordinator. This is simpler then automate the Iperf, but not so trivial.

--- modules/pingCoordinator/ping_coordinator.py
+++ modules/pingCoordinator/ping_coordinator.py
@@ -1,5 +1,6 @@
 import json
 import time
+import threading
 from datetime import datetime as dt
 from modules.mqttModule.mqtt_client import Mqtt_Client
 from bson import ObjectId
@@ -9,78 +10,80 @@ from modules.mongoModule.models.ping_result_model_mongo import PingResultModelMo
 
 class Ping_Coordinator:
 
-    def __init__(self, mqtt_client : Mqtt_Client, registration_handler_status, registration_handler_result, mongo_db : MongoDB):
+    def __init__(self, mqtt_client : Mqtt_Client, registration_handler_status, 
+                 registration_handler_result, registration_measure_preparer, mongo_db : MongoDB):
         self.mqtt_client = mqtt_client
         self.mongo_db = mongo_db
+        self.events_received_ack_from_probe_sender = {}
         self.last_mongo_measurement = None
 
-        # Requests to CommandsDemultiplexer
-        registration_response = registration_handler_status(
-            interested_status = "ping",
-            handler = self.handler_received_status)
+        # Requests to commands_multiplexer: handler STATUS registration
+        registration_response = registration_handler_status( interested_status = "ping",
+                                                             handler = self.handler_received_status)
         if registration_response == "OK" :
             print(f"Ping_Coordinator: registered handler for status -> ping")
         else:
             print(f"Ping_Coordinator: registration handler failed. Reason -> {registration_response}")
 
-        # Requests to CommandsDemultiplexer
-        registration_response = registration_handler_result(
-            interested_result = "ping",
-            handler = self.handler_received_result)
+        # Requests to commands_multiplexer: handler RESULT registration
+        registration_response = registration_handler_result(interested_result = "ping",
+                                                            handler = self.handler_received_result)
         if registration_response == "OK" :
             print(f"Ping_Coordinator: registered handler for result -> ping")
         else:
             print(f"Ping_Coordinator: registration handler failed. Reason -> {registration_response}")
 
+        # Requests to commands_multiplexer: Probes-Preparer registration
+        registration_response = registration_measure_preparer(
+            interested_measurement_type = "ping",
+            preparer = self.probes_preparer_to_measurements)
+        if registration_response == "OK" :
+            print(f"Ping_Coordinator: registered prepaper for measurements type -> ping")
+        else:
+            print(f"Ping_Coordinator: registration preparer failed. Reason -> {registration_response}")
+
 
     def handler_received_status(self, probe_sender, type, payload : json):
+        measure_id = payload["measurement_id"] if ("measurement_id" in payload) else None
+        command = payload["command"] if ("command" in payload) else None
         match type:
+            case "ACK":
+                if command == "start":
+                    self.events_received_ack_from_probe_sender[measure_id][1] = "OK"
+                    self.events_received_ack_from_probe_sender[measure_id][0].set()
             case "NACK":
-                command_failed_on_probe = payload["command"]
                 reason = payload['reason']
-                print(f"Ping_Coordinator: probe |{probe_sender}|->|{command_failed_on_probe}|->NACK, reason->{reason}")
+                self.events_received_ack_from_probe_sender[measure_id][1] = reason
+                self.events_received_ack_from_probe_sender[measure_id][0].set()
+                print(f"Ping_Coordinator: probe |{probe_sender}| -> |{command}| -> NACK, reason -> {reason}")
             case _:
                 print(f"Ping_Coordinator: received unkown type message -> |{type}|")
-        return
-    
-    def handler_received_result(self, probe_sender, result: json):
-        if ((time.time() - result["start_timestamp"]) < SECONDS_OLD_MEASUREMENT):
-            self.store_measurement_result(result = result)
-            self.mongo_db.set_measurement_as_completed(self.last_mongo_measurement._id)
-            self.print_summary_result(measurement_result = result)
-        else: #Volendo posso anche evitare questo settaggio, perchè ci penserà il thread periodico
-            #if self.mongo_db.set_measurement_as_failed_by_id(result['msm_id']):
-            print(f"Ping_Coordinator: ignored result. Reason: expired measurement -> {result['msm_id']}")
-    
 
-    def send_start_command(self, probe_sender, probe_receiver, destination_ip, source_ip, packets_number = 4, packets_size = 32):
-        self.last_mongo_measurement = MeasurementModelMongo(
-            description = "Latency measure with ping tool",
-            type = "Ping",
-            start_time = dt.now(),
-            source_probe = probe_sender,
-            dest_probe = probe_receiver,
-            source_probe_ip = source_ip,
-            dest_probe_ip = destination_ip)
-        self.last_mongo_measurement._id = self.mongo_db.insert_measurement(self.last_mongo_measurement)
-        if self.last_mongo_measurement._id is None:
-            print(f"Iperf_Coordinator: can't start ping. Error while storing ping measurement on Mongo")
+
+    def handler_received_result(self, probe_sender, result: json):
+        measure_id = result['msm_id'] if ('msm_id' in result) else None
+        if measure_id is None:
+            print("Ping_Coordinator: received result wihout measure_id -> IGNORED")
             return
         
+        if ((time.time() - result["start_timestamp"]) < SECONDS_OLD_MEASUREMENT):
+            if self.store_measurement_result(result = result):
+                print(f"Ping_Coordinator: complete the measurement store and update -> {measure_id}")
+        else: #Volendo posso anche evitare questo settaggio, perchè ci penserà il thread periodico
+            #if self.mongo_db.set_measurement_as_failed_by_id(result['msm_id']):
+            print(f"Ping_Coordinator: ignored result. Reason: expired measurement -> {measure_id}")
+
+
+    def send_probe_ping_start(self, probe_sender, json_payload):
         json_ping_start = {
             "handler": "ping",
             "command": "start",
-            "payload": {
-                "destination_ip": destination_ip,
-                "msm_id": str(self.last_mongo_measurement._id),
-                "packets_number": packets_number,
-                "packets_size": packets_size
-            }
+            "payload": json_payload
         }
         self.mqtt_client.publish_on_command_topic(probe_id = probe_sender, complete_command=json.dumps(json_ping_start))
 
 
-    def send_stop_command(self, probe_destination):
+    def send_probe_ping_stop(self, probe_destination):
         json_ping_stop = {
             "handler": "ping",
             "command": "stop",
@@ -89,8 +92,8 @@ class Ping_Coordinator:
         self.mqtt_client.publish_on_command_topic(probe_id=probe_destination, complete_command=json.dumps(json_ping_stop))
 
 
-    def store_measurement_result(self, result : json):
-        mongo_result = PingResultModelMongo(
+    def store_measurement_result(self, result : json) -> bool:
+        ping_result = PingResultModelMongo(
             msm_id = ObjectId(result["msm_id"]),
             start_timestamp = result["start_timestamp"],
             rtt_avg = result["rtt_avg"],
@@ -103,11 +106,17 @@ class Ping_Coordinator:
             packets_loss_rate = result["packet_loss_rate"],
             icmp_replies = result["icmp_replies"]
         )
-        result_id = str(self.mongo_db.insert_ping_result(result=mongo_result))
+        result_id = str(self.mongo_db.insert_ping_result(result = ping_result))
         if result_id is not None:
+            measure_id = result["msm_id"]
             print(f"Iperf_Coordinator: result |{result_id}| stored in db")
-        else:
-            print(f"Iperf_Coordinator: error while storing result |{result_id}|")
+            if self.mongo_db.update_results_array_in_measurement(measure_id):
+                print(f"Ping_Coordinator: updated document linking in measure: |{measure_id}|")
+                if self.mongo_db.set_measurement_as_completed(measure_id):
+                    self.print_summary_result(measurement_result = result)
+                    return True
+        print(f"Iperf_Coordinator: error while storing result |{result_id}|")
+        return False
 
 
     def print_summary_result(self, measurement_result):
@@ -140,3 +149,65 @@ class Ping_Coordinator:
         for icmp_reply in measurement_result['icmp_replies']:
             if "destination" in icmp_reply:
                 print(f"\t-------------------------\n{icmp_reply}\n")
+
+
+    """    # Questo metodo è quello che diventerà il PREPARER
+    def send_start_command(self, probe_sender, probe_receiver, destination_ip, source_ip, packets_number = 4, packets_size = 32):
+        self.last_mongo_measurement = MeasurementModelMongo(
+            description = "Latency measure with ping tool",
+            type = "ping",
+            start_time = dt.now(),
+            source_probe = probe_sender,
+            dest_probe = probe_receiver,
+            source_probe_ip = source_ip,
+            dest_probe_ip = destination_ip)
+        self.last_mongo_measurement._id = self.mongo_db.insert_measurement(self.last_mongo_measurement)
+        if self.last_mongo_measurement._id is None:
+            print(f"Iperf_Coordinator: can't start ping. Error while storing ping measurement on Mongo")
+            return
+        
+        json_ping_start = {
+            "handler": "ping",
+            "command": "start",
+            "payload": {
+                "destination_ip": destination_ip,
+                "msm_id": str(self.last_mongo_measurement._id),
+                "packets_number": packets_number,
+                "packets_size": packets_size
+            }
+        }
+        self.mqtt_client.publish_on_command_topic(probe_id = probe_sender, complete_command=json.dumps(json_ping_start))
+    """
+
+
+    def probes_preparer_to_measurements(self, new_measurement : MeasurementModelMongo):
+        DEFAULT_PACKET_NUMBER = 4
+        DEFAULT_PACKET_SIZE = 32
+        new_measurement.assign_id()
+        measurement_id = str(new_measurement._id)
+
+        json_start_payload = {
+                "destination_ip": new_measurement.dest_probe_ip,
+                "msm_id": measurement_id,
+                "packets_number": DEFAULT_PACKET_NUMBER,
+                "packets_size": DEFAULT_PACKET_SIZE }
+        
+        self.events_received_ack_from_probe_sender[measurement_id] = [threading.Event(), None]
+        self.send_probe_ping_start(probe_sender = new_measurement.source_probe, json_payload=json_start_payload)
+
+        self.events_received_ack_from_probe_sender[measurement_id][0].wait(timeout = 5)
+        # ------------------------------- YOU MUST WAIT (AT MOST 5s) FOR AN ACK/NACK FROM SENDER_PROBE (PING INITIATOR)
+
+        probe_sender_event_message = self.events_received_ack_from_probe_sender[measurement_id][1]
+        if probe_sender_event_message == "OK": # If the iperf-server configuration went good, then...
+            inserted_measurement_id = self.mongo_db.insert_measurement(self.last_mongo_measurement)
+            if inserted_measurement_id is None:
+                print(f"Iperf_Coordinator: can't start ping. Error while storing ping measurement on Mongo")
+                return "Error", "Can't send start! Error while inserting measurement ping in mongo", "MongoDB Down?"
+            return "OK", new_measurement.to_dict(), None
+        elif probe_sender_event_message is not None:
+            print(f"Preparer ping: awaked from server conf NACK -> {probe_sender_event_message}")
+            return "Error", f"Probe |{new_measurement.source_probe}| says: {probe_sender_event_message}", "State BUSY"            
+        else:
+            print(f"Preparer ping: No response from probe -> |{new_measurement.source_probe}")
+            return "Error", f"No response from Probe: {new_measurement.source_probe}" , "Reponse Timeout"
