0a48ecc50d8f0cb78fcc12f00afd988f7e176a23
francescoB1997 <francesco.0297@hotmail.it>
2025-02-10 19:59:29 +0100

    The CommandsDemultiplexer must known the mqtt instance + This is due to the internal default handler for message whose type is "root_service". This handler, actually, handles only one type of command, the "set_coordinator_ip" command, usefull to save the coordinator ip for future NTP sync.

--- probesFirmware/commandsDemultiplexer/commandsDemultiplexer.py
+++ probesFirmware/commandsDemultiplexer/commandsDemultiplexer.py
@@ -1,4 +1,7 @@
 import json
+import threading
+from probesFirmware.mqttModule.mqttClient import ProbeMqttClient
+from shared_resources import shared_state
 
 class CommandsDemultiplexer():
     def __init__(self):
@@ -10,11 +13,14 @@ class CommandsDemultiplexer():
         self.event_to_set_in_case_of_root_service_command_reception = None
         #self.status_handler_list = {} # For the future. Is necessary that the probe knows about the Coordinator state? Or another probe state?
 
+    def set_mqtt_client(self, mqtt_client : ProbeMqttClient):
+        self.mqtt_client = mqtt_client
+
     # +------------------ commands_handler_list ------------------+
     # |      COMMAND     |          HANDLER FUNCTION              |
     # +------------------+----------------------------------------+
     # |       iperf      |  iperfController.iperf_command_handler |
-    # |       ping       |                                        |
+    # |       ping       |  pingController.ping_command_handler   |
     # |   bg_generator   |                                        |
     # +------------------+----------------------------------------+
 
@@ -40,3 +46,25 @@ class CommandsDemultiplexer():
             self.commands_handler_list[handler](command, payload)
         else:
             print(f"CommandsDemultiplexer: no registered handler for |{handler}|")
+
+    def root_service_command_handler(self, command, payload):
+        global shared_state
+        match command:
+            case "set_coordinator_ip":
+                coordinator_ip = str(payload['coordinator_ip'])
+                shared_state.set_coordinator_ip(coordinator_ip = coordinator_ip)
+                print(f"CommandsDemultiplexer: coordinator_ip received -> {shared_state.get_coordinator_ip()}")
+                if self.event_to_set_in_case_of_root_service_command_reception is not None:
+                    self.event_to_set_in_case_of_root_service_command_reception.set()
+            case _:
+                print(f"CommandsDemultiplexer: root_service handler -> Unkown command -> {command}")
+    
+    def wait_for_set_coordinator_ip(self):
+        # This is a BLOCKING METHOD
+        self.event_to_set_in_case_of_root_service_command_reception = threading.Event()
+        self.mqtt_client.publish_probe_state("UPDATE")
+        #print("CommandsDemultiplexer: waiting for coordinator_ip reception ...")
+        self.event_to_set_in_case_of_root_service_command_reception.wait(timeout = 5) 
+        # ------------------------- WAIT for ROOT_SERVICE command RECEPTION from COORDINATOR -------------------------
+        self.event_to_set_in_case_of_root_service_command_reception = None
+
