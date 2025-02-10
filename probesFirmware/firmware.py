126be35babaafbea0341a4d60d0aa41b117b14ef
francescoB1997 <francesco.0297@hotmail.it>
2025-02-10 20:01:19 +0100

    Added the AgeOfInformation module to the probesFirmware

--- probesFirmware/firmware.py
+++ probesFirmware/firmware.py
@@ -3,7 +3,9 @@ from mqttModule.mqttClient import ProbeMqttClient
 from commandsDemultiplexer.commandsDemultiplexer import CommandsDemultiplexer
 from iperfModule.iperfController import IperfController
 from pingModule.pingController import PingController
-from energyModule.energyController import EnergyController
+#from energyModule.energyController import EnergyController
+from aoiModule.aoiController import AgeOfInformationController
+from shared_resources import shared_state
 
 
 class Probe:
@@ -13,16 +15,29 @@ class Probe:
         self.commands_demultiplexer = CommandsDemultiplexer()
         self.mqtt_client = ProbeMqttClient(probe_id,
                                            self.commands_demultiplexer.decode_command) # The Decode Handler is triggered internally
-        
+        self.commands_demultiplexer.set_mqtt_client(self.mqtt_client)
+
         self.iperf_controller = IperfController(self.mqtt_client,
                                                 self.commands_demultiplexer.registration_handler_request) # ENABLE THROUGHPUT FUNCTIONALITY
         
         self.ping_controller = PingController(self.mqtt_client,
                                               self.commands_demultiplexer.registration_handler_request)   # ENABLE LATENCY FUNCTIONALITY
         
-        self.energy_controller = EnergyController(self.mqtt_client,
-                                                  self.commands_demultiplexer.registration_handler_request) # ENABLE POWER CONSUMPTION FUNCTIONALITY
+        #self.energy_controller = EnergyController(self.mqtt_client,
+        #                                          self.commands_demultiplexer.registration_handler_request) # ENABLE POWER CONSUMPTION FUNCTIONALITY
         
+        self.aoi_controller = AgeOfInformationController(self.mqtt_client, 
+                                                         self.commands_demultiplexer.registration_handler_request,
+                                                         self.commands_demultiplexer.wait_for_set_coordinator_ip)
+        
+    def invoke_test(self):
+        global shared_state
+        if shared_state.coordinator_ip is None:
+            self.commands_demultiplexer.wait_for_set_coordinator_ip()
+            if shared_state.get_coordinator_ip() is None: # Necessary check for confirm the coordinator response of coordinator_ip
+                print("No response from coordinator. Missing coordinator ip for root service")
+        else:
+            print(f"Already known the coordinator ip -> {shared_state.get_coordinator_ip()}")
 
     def check_for_ready(self):
         return self.state
@@ -34,7 +49,7 @@ class Probe:
 
 def main():
     user_name = os.getlogin()
-    if user_name == "coordinator": # Trick for execute the firmware on the coordinator
+    if user_name == "coordinator" or user_name=="Francesco": # Trick for execute the firmware on the coordinator
         user_name = "probe1"
     probe1 = Probe(user_name)
     while True:
@@ -44,7 +59,7 @@ def main():
                 probe1.disconnect()
                 break
             case "1":
-                probe1.mqtt_client.publish_probe_state(state="ONLINE")
+                probe1.invoke_test()
             case _:
                 continue  
     return
