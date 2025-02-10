a2c61f26b121a008a152c24ba33e9b35df8b47af
francescoB1997 <francesco.0297@hotmail.it>
2025-02-10 19:51:31 +0100

    Corrected the Singleton class

--- probesFirmware/shared_resources.py
+++ probesFirmware/shared_resources.py
@@ -4,9 +4,13 @@ BUSY = "BUSY"
 READY = "READY"
 
 class SharedState:
-    def __init__(self):
-        self.lock = threading.Lock()
-        self.probe_state = READY
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(SharedState, cls).__new__(cls)
+            cls.instance.lock = threading.Lock()
+            cls.instance.probe_state = READY
+            cls.instance.coordinator_ip = None
+        return cls.instance
 
     def set_probe_as_ready(self) -> bool:
         with self.lock:
@@ -26,5 +30,17 @@ class SharedState:
     def probe_is_ready(self) -> bool:
         with self.lock:
             return (self.probe_state == READY)
+        
+    
+    def set_coordinator_ip(self, coordinator_ip):
+        with self.lock:
+            if (self.coordinator_ip is None):
+                self.coordinator_ip = coordinator_ip
+                print(f"SharedState: setted coordinator ip -> {self.coordinator_ip}")
+
+    def get_coordinator_ip(self):
+        with self.lock:
+            return self.coordinator_ip
+
 
 shared_state = SharedState() # my singleton shared
