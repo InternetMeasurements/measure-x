068d9541a16b8ad2dc8d7c7027662db5276f4ae2
francescoB1997 <francesco.0297@hotmail.it>
2025-02-10 19:59:04 +0100

    The CommandsDemultiplexer must known the mqtt instance + This is due to the internal default handler for message whose type is "root_service". This handler, actually, handles only one type of command, the "set_coordinator_ip" command, usefull to save the coordinator ip for future NTP sync.

--- probesFirmware/commandsDemultiplexer/commandsDemultiplexer.py
+++ probesFirmware/commandsDemultiplexer/commandsDemultiplexer.py
@@ -3,6 +3,11 @@ import json
 class CommandsDemultiplexer():
     def __init__(self):
         self.commands_handler_list = {}
+        # The CommandsDemultiplexer set as a handler for "root_service" commands, an its internal method
+        self.registration_handler_request(interested_command="root_service", handler=self.root_service_command_handler)
+
+        self.mqtt_client = None
+        self.event_to_set_in_case_of_root_service_command_reception = None
         #self.status_handler_list = {} # For the future. Is necessary that the probe knows about the Coordinator state? Or another probe state?
 
     # +------------------ commands_handler_list ------------------+
