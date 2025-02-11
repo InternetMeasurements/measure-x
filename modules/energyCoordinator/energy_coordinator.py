d90d897cbe316200810b619b0d0a9f3afbce9534
francescoB1997 <francesco.0297@hotmail.it>
2025-02-11 16:30:10 +0100

    Added probe_preparer method in EnergyCoordinator + Added probe_preparer registration to the CommandDemultiplexer in the __init__ method + This preparer method will be invoked by REST API, when is necessary to measure the power consumption + In the handler_received_status(), added the handling of the ACK and NACK from probes + Used the events_received_start_ack dict and the queued_measurements dict to handle more than one measure at time

--- modules/energyCoordinator/energy_coordinator.py
+++ modules/energyCoordinator/energy_coordinator.py
@@ -1,25 +1,20 @@
 import json
+import threading
 from modules.mongoModule.mongoDB import MongoDB
 from modules.mqttModule.mqtt_client import Mqtt_Client
+from modules.mongoModule.models.measurement_model_mongo import MeasurementModelMongo
 
 class EnergyCoordinator:
     def __init__(self, 
                  mqtt_client : Mqtt_Client,
-                 registration_handler_error,
                  registration_handler_status,
                  registration_handler_result,
+                 registration_measure_preparer,
                  mongo_db : MongoDB):
         self.mqtt_client = mqtt_client
         self.mongo_db = mongo_db
-
-        # Requests to CommandsDemultiplexer
-        registration_response = registration_handler_error(
-            interested_error = "energy",
-            handler = self.handler_error_messages)
-        if registration_response == "OK" :
-            print(f"Energy_Coordinator: registered handler for error -> energy")
-        else:
-            print(f"Energy_Coordinator: registration handler failed. Reason -> {registration_response}")
+        self.queued_measurements = {}
+        self.events_received_start_ack = {}
 
          # Requests to CommandsDemultiplexer
         registration_response = registration_handler_status(
@@ -35,7 +30,16 @@ class EnergyCoordinator:
             interested_result = "energy",
             handler = self.handler_received_result)
         if registration_response == "OK" :
-            print(f"Energy_Coordinator: registered handler for result -> energy")
+            print(f"EnergyCoordinator: registered handler for result -> energy")
+        else:
+            print(f"EnergyCoordinator: registration handler failed. Reason -> {registration_response}")
+
+        # Requests to commands_multiplexer: Probes-Preparer registration
+        registration_response = registration_measure_preparer(
+            interested_measurement_type = "energy",
+            preparer = self.probes_preparer_to_measurements)
+        if registration_response == "OK" :
+            print(f"EnergyCoordinator: registered prepaper for measurements type -> energy")
         else:
             print(f"EnergyCoordinator: registration preparer failed. Reason -> {registration_response}")
         
@@ -43,7 +47,26 @@ class EnergyCoordinator:
         print(f"EnergyCoordinator: received error msg from |{probe_sender}| --> |{payload}|")
     
     def handler_received_status(self, probe_sender, type, payload):
-        print(f"Energy_Coordinator: status sender->|{probe_sender}| type->|{type}| payload->|{payload}|")
+        match type:
+            case "ACK":
+                command_executed_on_probe = payload["command"]
+                measurement_id = payload["msm_id"] if ("msm_id" in payload) else None
+                match command_executed_on_probe:
+                    case "check":
+                        print(f"EnergyCoordinator: received ACK related to |check| from |{probe_sender}|")
+                    case "start":
+                        if measurement_id is None:
+                            print(f"EnergyCoordinator: received ACK related to |start| from {probe_sender} WITHOUT msm_id")
+                            return
+                        print(f"EnergyCoordinator:")
+            case "NACK":
+                failed_command = payload["command"]
+                reason = payload["reason"]
+                measurement_id = payload["msm_id"] if ("msm_id" in payload) else None
+                print(f"EnergyCoordinator: NACK from probe ->|{probe_sender}| reason ->|{reason}| msm_id ->|{measurement_id}|")
+                if measurement_id is not None:
+                    self.events_received_start_ack[measurement_id][1] = reason
+                    self.events_received_start_ack[measurement_id][0].set()
 
     def handler_received_result(self, probe_sender, result: json):
         print(f"EnergyCoordinator: result received from {probe_sender}")
@@ -59,20 +82,43 @@ class EnergyCoordinator:
         self.mqtt_client.publish_on_command_topic(probe_id = probe_id, 
                                                   complete_command = json.dumps(json_check_i2C_command))
         
-    def send_start_command(self, probe_id):
-        json_energy_start = {
+    def probes_preparer_to_measurements(self, new_measurement : MeasurementModelMongo):
+        new_measurement.assign_id()
+        measurement_id = str(new_measurement._id)
+        self.events_received_start_ack[measurement_id] = [threading.Event(), None]
+        self.queued_measurements[str(new_measurement._id)] = new_measurement
+
+        json_iperf_start = {
             "handler": "energy",
             "command": "start",
-            "payload": {}
+            "payload": {
+                "msm_id": measurement_id
+            }
         }
-        self.mqtt_client.publish_on_command_topic(probe_id = probe_id,
-                                                  complete_command=json.dumps(json_energy_start))
-    
-    def send_stop_command(self, probe_id):
+        self.events_received_start_ack[measurement_id] = [threading.Event(), None]
+        self.mqtt_client.publish_on_command_topic(probe_id = new_measurement.source_probe, complete_command = json.dumps(json_iperf_start))
+        self.events_received_start_ack[measurement_id][0].wait(timeout = 5)
+        # ------------------------------- YOU MUST WAIT (AT MOST 5s) FOR AN ACK/NACK FROM DEST_PROBE
+        probe_event_message = self.events_received_start_ack[measurement_id][1]
+        if probe_event_message == "OK":
+            measurement_id = self.mongo_db.insert_measurement(new_measurement)
+            if (measurement_id is None):
+                return "Error", "Can't store measure in Mongo! Error while inserting measurement energy in mongo", "MongoDB Down?"
+            return "OK", new_measurement.to_dict(), None # By returning these arguments, it's possible to see them in the HTTP response
+        elif probe_event_message is not None:
+            print(f"Preparer energy: awaked from probe energy NACK -> {probe_event_message}")
+            return "Error", f"Probe |{new_measurement.source_probe}| says: {probe_event_message}", ""
+        else:
+            print(f"Preparer energy: No response from probe -> |{new_measurement.source_probe}")
+            return "Error", f"No response from Probe: {new_measurement.source_probe}" , "Response Timeout"
+
+    def send_stop_command(self, probe_id, msm_id):
         json_energy_stop = {
             "handler": "energy",
             "command": "stop",
-            "payload": {}
+            "payload": {
+                "msm_id": msm_id
+            }
         }
         self.mqtt_client.publish_on_command_topic(probe_id = probe_id,
                                                   complete_command=json.dumps(json_energy_stop))
