03ac3dcb0ba199f2d0182fe72dbdf167d82e69c3
francescoB1997 <francesco.0297@hotmail.it>
2025-02-11 16:36:44 +0100

    Implemented the EnergyController on probe side + This module is responsile for energy measurements + Defined the method for ACK, NACK and the method that will check if the probe has the INA sensor

--- probesFirmware/energyModule/energyController.py
+++ probesFirmware/energyModule/energyController.py
@@ -10,7 +10,7 @@ class EnergyController:
             self.driverINA = Ina219Driver()
         except Exception as e:
             self.mqtt_client.publish_error(handler="energy", payload="No energy sensor provided")
-            print(f"{e}")
+            print(f"EnergyController: No energy sensor provided")
             return
         
         # Requests to commands_multiplexer
@@ -20,34 +20,63 @@ class EnergyController:
         if registration_response != "OK" :
             self.mqtt_client.publish_error(handler = "energy", payload = registration_response)
             print(f"EnergyController: registration handler failed. Reason -> {registration_response}")
-        #self.mqtt_client.publish_command_ACK(handler="energy", payload="OK")
 
         
     def energy_command_handler(self, command : str, payload: json):
         print(f"EnergyController: command -> {command} | payload-> {payload}")
         match command:
             case "check":
-                if self.driverINA.i2C_INA_check():
-                    check_msg = "i2C INA219 Found"
+                if self.INA_sensor_test() == "PASSED":
                     SYNC_OTII_PIN.on()
-                    self.mqtt_client.publish_command_ACK(handler = "energy", payload = check_msg)
+                    self.send_energy_ACK(successed_command="check")
                     SYNC_OTII_PIN.off()
                 else:
-                    check_msg = "i2C INA219 NOT Found"
-                    self.mqtt_client.publish_command_NACK(handler = "energy", payload = check_msg)
+                    self.send_energy_NACK(failed_command="start", error_info="INA219 NOT FOUND", measurement_id=msm_id)
             case "start":
-                    msm_id = payload['msm_id']
+                    msm_id = payload['msm_id'] if 'msm_id' in payload else None
+                    if msm_id is None:
+                        self.send_energy_NACK(failed_command="start", error_info="No measurement provided")
+                        return
+                    if self.INA_sensor_test() != "PASSED":
+                        self.send_energy_NACK(failed_command="start", error_info="INA219 NOT FOUND", measurement_id=msm_id)
+                        return
                     start_msg = self.driverINA.start_current_measurement(filename = msm_id)
                     if start_msg != "OK":
-                         self.mqtt_client.publish_command_NACK(handler = "energy", payload = start_msg)
+                        self.send_energy_NACK(failed_command="start", error_info=start_msg, measurement_id=msm_id)
                     else:
-                         self.mqtt_client.publish_command_ACK(handler = "energy", payload = command)
+                        self.send_energy_ACK(successed_command="start", measurement_id=msm_id)
             case "stop":
+                    msm_id = payload['msm_id'] if 'msm_id' in payload else None
+                    if msm_id is None:
+                        self.send_energy_NACK(failed_command="stop", error_info="No measurement provided")
+                        return
                     stop_msg = self.driverINA.stop_current_measurement()
                     if stop_msg != "OK":
-                         self.mqtt_client.publish_command_NACK(handler="energy", payload=stop_msg)
+                        self.send_energy_NACK(failed_command="stop", error_info=stop_msg, measurement_id=msm_id)
                     else:
-                         self.mqtt_client.publish_command_ACK(handler="energy", payload=command)
+                        self.send_energy_ACK(successed_command="stop", measurement_id=msm_id)
             case _:
                 print(f"EnergyController: unkown command -> {command}")
+                self.send_energy_NACK(failed_command=command, error_info="Unknown command")
+
+    def INA_sensor_test(self):
+        test_sensor = self.driverINA.i2C_INA_check()
+        return "PASSED" if test_sensor else "NOT PASSED"
+
+    def send_energy_ACK(self, successed_command, measurement_id = None):
+        json_ack = { 
+            "command" : successed_command,
+            "msm_id" : self.last_measurement_id if (measurement_id is None) else measurement_id
+            }
+        print(f"EnergyController: ACK sending -> {json_ack}")
+        self.mqtt_client.publish_command_ACK(handler='energy', payload=json_ack) 
+    
+    def send_energy_NACK(self, failed_command, error_info, measurement_id = None):
+        json_nack = {
+            "command" : failed_command,
+            "reason" : error_info,
+            "msm_id" : self.last_measurement_id if (measurement_id is None) else measurement_id
+            }
+        print(f"EnergyController: NACK sending -> {json_nack}")
+        self.mqtt_client.publish_command_NACK(handler='energy', payload = json_nack) 
         
