ea6008d2427cc49e2ebf8ec4da2c5dd90d7ccecf
francescoB1997 <francesco.0297@hotmail.it>
2025-02-12 12:11:27 +0100

    Energy Result Model Mongo + This class represent the data model of a measurement result of type Energy

---
+++ modules/mongoModule/models/energy_result_model_mongo.py
@@ -0,0 +1,22 @@
+from bson import ObjectId
+
+class EnergyResultModelMongo:
+    def __init__(self, msm_id : str, timeseries, energy = None, byte_tx = None, byte_rx = None, duration = None):
+        self._id = None
+        self.msm_id = msm_id
+        self.timeseries = timeseries
+        self.energy = energy
+        self.byte_tx = byte_tx
+        self.byte_rx = byte_rx
+        self.duration = duration
+
+
+    def to_dict(self):
+        return {
+            "msm_id": ObjectId(self.msm_id) if isinstance(self.msm_id, str) else self.msm_id,
+            "timeseries": self.timeseries,
+            "energy": self.energy,
+            "byte_tx": self.byte_tx,
+            "byte_rx": self.byte_rx,
+            "duration": self.duration,
+        }
