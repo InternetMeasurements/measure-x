73a476e339699247e82e59d9e791a5b38fabb1ec
francescoB1997 <francesco.0297@hotmail.it>
2025-02-18 13:05:06 +0100

    Handled Iperf Stop Command + Both on the coordinator and on probe side, i've handled the stop command execution and notification

--- coordinator.py
+++ coordinator.py
@@ -59,6 +59,7 @@ def main():
         registration_handler_status = commands_multiplexer.add_status_handler,
         registration_measure_preparer = commands_multiplexer.add_probes_preparer,
         ask_probe_ip = commands_multiplexer.ask_probe_ip,
+        registration_measurement_stopper=commands_multiplexer.add_measurement_stopper,
         mongo_db=mongo_db)
     
     ping_coordinator = Ping_Coordinator(
