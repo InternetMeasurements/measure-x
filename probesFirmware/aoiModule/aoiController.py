803adfdf26b9c0b83169ba53658dd004cef3f5f6
francescoB1997 <francesco.0297@hotmail.it>
2025-02-20 20:22:18 +0100

    Implemented AoI Module on Probe side + As we knoe, also the probe side has a AoI module, resposible to handle the commands related to Age of Information metric. + The true measurement is performed in a separated thread in order to detach this computation from the main thread,

--- probesFirmware/aoiModule/aoiController.py
+++ probesFirmware/aoiModule/aoiController.py
@@ -11,6 +11,7 @@ class AgeOfInformationController:
         self.mqtt_client = mqtt_client
         self.aoi_thread = None
         self.last_measurement_id = None
+        self.last_probe_ntp_server_ip = None
         self._continue = False
 
         self.wait_for_set_coordinator_ip = wait_for_set_coordinator_ip
@@ -24,33 +25,58 @@ class AgeOfInformationController:
 
 
     def aoi_command_handler(self, command : str, payload: json):
+         msm_id = payload["msm_id"] if "msm_id" in payload else None
+         if msm_id is None:
+             self.send_aoi_NACK(failed_command=command, error_info="No measurement_id provided")
+             return
          match command:
             case "start":
-                msm_id = payload['msm_id']
                 if not shared_state.set_probe_as_ready():
-                    self.send_aoi_NACK(failed_command="start", error_info="PROBE BUSY", measurement_id=msm_id)
+                    self.send_aoi_NACK(failed_command=command, error_info="PROBE BUSY", msm_id=msm_id)
                     return
-                
                 if shared_state.get_coordinator_ip() is None:
                     self.wait_for_set_coordinator_ip()
                     if shared_state.get_coordinator_ip() is None: # Necessary check for confirm the coordinator response of coordinator_ip
-                        self.send_aoi_NACK(failed_command="start", error_info = "No response from coordinator. Missing coordinator ip for root service", measurement_id=msm_id)
+                        self.send_aoi_NACK(failed_command="start", error_info = "No response from coordinator. Missing coordinator ip for root service", msm_id=msm_id)
                         return
-                
-                self.send_aoi_ACK(successed_command="start", measurement_id=msm_id)
-                self._continue = True
-                self.aoi_thread = threading.Thread(target=self.run_aoi_measurement, args=(msm_id))
-                self.aoi_thread.start()
+                self.prepare_probe_to_start_aoi_measure(msm_id=msm_id)
+            case "disable_ntp_service":
+                if not shared_state.set_probe_as_ready():
+                    self.send_aoi_NACK(failed_command=command, error_info="PROBE BUSY", msm_id=msm_id)
+                    return
+                probe_ntp_server_ip = payload["probe_ntp_server"] if "probe_ntp_server" in payload else None
+                if probe_ntp_server_ip is None:
+                    self.send_aoi_NACK(failed_command=command, error_info="No probe-ntp-server provided", msm_id=msm_id)
+                    return
+                disable_msg = self.stop_ntpsec_service(msm_id = msm_id, probe_ntp_server_ip = probe_ntp_server_ip)
+                if disable_msg == "OK":
+                    self.send_aoi_ACK(successed_command = command, msm_id = msm_id)
+                else:
+                    self.send_aoi_NACK(failed_command = command, error_info = disable_msg, msm_id = msm_id)
+            case "enable_ntp_service":
+                if not shared_state.set_probe_as_ready():
+                    self.send_aoi_NACK(failed_command=command, error_info="PROBE BUSY", msm_id=msm_id)
+                    return
+                enable_msg = self.start_ntpsec_service()
+                if enable_msg == "OK":
+                    self.send_aoi_ACK(successed_command = command, msm_id = msm_id)
+                else:
+                    self.send_aoi_NACK(failed_command = command, error_info = enable_msg, msm_id = msm_id)
             case "stop":
                 msm_id = payload['msm_id']
                 termination_message = self.stop_aoi_thread()
                 if termination_message == "OK":
-                    self.send_aoi_ACK(successed_command=command, measurement_id=msm_id)
+                    self.send_aoi_ACK(successed_command=command, msm_id=msm_id)
                 else:
                     self.send_aoi_NACK(failed_command=command, error_info=termination_message)
                 self.reset_vars()
-                
-                
+                                
+
+    def prepare_probe_to_start_aoi_measure(self, msm_id):
+        self._continue = True
+        self.aoi_thread = threading.Thread(target=self.run_aoi_measurement, args=(msm_id))
+        self.aoi_thread.start()
+
 
     def run_aoi_measurement(self, msm_id):
         result = subprocess.run( ['sudo', 'ntpdate', shared_state.get_coordinator_ip()], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -59,34 +85,59 @@ class AgeOfInformationController:
                 current_time_utc = datetime.now(timezone.utc)
                 print(f"Current UTC time: {current_time_utc}")
                 threading.sleep(1)
+                
         else:
-            self.send_aoi_NACK(failed_command="start", error_info=result.stderr.decode('utf-8'), measurement_id=msm_id)
+            self.send_aoi_NACK(failed_command="start", error_info=result.stderr.decode('utf-8'), msm_id=msm_id)
+
 
     def stop_aoi_thread(self) -> str:
         if self.aoi_thread is None:
             return "Process NTPDATE not in Execution"
         self._continue = False
         return "OK"
+    
 
+    def stop_ntpsec_service(self, msm_id, probe_ntp_server_ip):
+        stop_command = [ "systemctl stop ntpsec" ] #[ "sudo systemctl stop ntpsec" ]
+        
+        result = subprocess.run(stop_command.split(), stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+        if result.returncode != 0:
+            stdout_stop_command = result.stdout.decode('utf-8')
+            return f"Error in stopping ntsec service. Return code: {result.returncode}. STDOUT: {stdout_stop_command}"
+        self.last_probe_ntp_server_ip = probe_ntp_server_ip
+        self.last_measurement_id = msm_id
+        return "OK"
+    
+    
+    def start_ntpsec_service(self):
+        start_command = [ "systemctl start ntpsec" ] #[ "sudo systemctl start ntpsec" ]
+        
+        result = subprocess.run(start_command.split(), stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+        if result.returncode != 0:
+            stdout_stop_command = result.stdout.decode('utf-8')
+            return f"Error in staring ntsec service. Return code: {result.returncode}. STDOUT: {stdout_stop_command}"
+        return "OK"
+    
 
-
-    def send_aoi_ACK(self, successed_command, measurement_id = None):
+    def send_aoi_ACK(self, successed_command, msm_id = None):
         json_ack = { 
             "command" : successed_command,
-            "measurement_id" : self.last_measurement_id if (measurement_id is None) else measurement_id
+            "measurement_id" : self.last_measurement_id if (msm_id is None) else msm_id
             }
         print(f"AoIController: ACK sending -> {json_ack}")
         self.mqtt_client.publish_command_ACK(handler='aoi', payload=json_ack) 
-    
-    def send_aoi_NACK(self, failed_command, error_info, measurement_id = None):
+
+
+    def send_aoi_NACK(self, failed_command, error_info, msm_id = None):
         json_nack = {
             "command" : failed_command,
             "reason" : error_info,
-            "measurement_id" : self.last_measurement_id if (measurement_id is None) else measurement_id
+            "msm_id" : self.last_measurement_id if (msm_id is None) else msm_id
             }
         print(f"AoIController: NACK sending -> {json_nack}")
         self.mqtt_client.publish_command_NACK(handler='aoi', payload = json_nack) 
-    
+
+
     def reset_vars(self):
         self.last_measurement_id = None
         self.aoi_thread = None
