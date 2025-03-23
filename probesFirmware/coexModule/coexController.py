b51c252758753c6addc057184676b3a1fa73e3ac
francescoB1997 <francesco.0297@hotmail.it>
2025-03-23 18:52:03 +0100

    Minor test

--- probesFirmware/coexModule/coexController.py
+++ probesFirmware/coexModule/coexController.py
@@ -245,8 +245,8 @@ class CoexController:
                                        self.last_coex_parameters.counterpart_probe_mac, str(self.last_coex_parameters.socker_port) ]
                     self.tcpliveplay_process = subprocess.Popen(tcpliveplay_cmd, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL, text = True)
                     print(f"Thread_Coex: tcpliveplay started")
-                    #time.sleep(10)
-                    #self.tcpliveplay_process.terminate()
+                    time.sleep(20)
+                    self.tcpliveplay_process.terminate()
                     self.tcpliveplay_process.wait()
                     print(f"Thread_Coex: tcpliveplay killed")
                     
