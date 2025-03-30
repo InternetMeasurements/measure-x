72fbf6d7f4d09c20d61f14d94fdf4729a5227abe
francescoB1997 <francesco.0297@hotmail.it>
2025-03-30 15:35:36 +0200

    Added delay to sample trace with 5s delay each repetition

--- probesFirmware/iperfModule/iperfController.py
+++ probesFirmware/iperfModule/iperfController.py
@@ -162,14 +162,14 @@ class IperfController:
         repetition_count = 0
         execution_return_code = -2
         while (repetition_count < self.repetitions):
-            time.sleep(0.5)
+            time.sleep(2.5)
             print(f"\n*************** Repetition: {repetition_count + 1} ***************")
             execution_return_code = self.run_iperf_execution()
             if execution_return_code != 0: # 0 is the correct execution code
                 break
             self.publish_last_output_iperf(repetition = repetition_count, last_result=((repetition_count + 1) == self.repetitions))
             repetition_count += 1
-            time.sleep(0.5)
+            time.sleep(2.5)
 
         if (execution_return_code != 0) and (execution_return_code != signal.SIGTERM):
             self.send_iperf_NACK(failed_command="start", error_info=self.last_error, msm_id=self.last_measurement_id)
